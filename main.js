0befbc548f5ce7c254eaa2395e4be3bccbd48484
Colton <czpemberton@gmail.com>
2018-12-04 12:43:19 -0800

    Updates

---
+++ main.js
@@ -0,0 +1,24 @@
+const toggle = document.querySelector('#toggle');
+const nav = document.querySelector('#navigation')
+const ul = document.querySelector('#topMenu');
+const navItems = document.querySelectorAll('a');
+
+function toggleNav(){
+    if (nav.className == 'closed'){
+        nav.className = 'open';
+    } else {
+        nav.className = 'closed';
+    }
+};
+
+toggle.addEventListener('click', (event) => {
+    toggleNav();
+});
+
+nav.addEventListener('click', (event) => {
+    for (let i = 0; i < navItems.length; i++) {
+        if (event.target == navItems[i]){
+            toggleNav();
+        }
+    }
+});
