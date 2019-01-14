dc6ded023a65a751a10c7011b38f52df585fa032
Colton <czpemberton@gmail.com>
2019-01-13 19:56:27 -0800

    Updates

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const nav = document.querySelector('#navigation')
 const ul = document.querySelector('#topMenu');
 const navItems = document.querySelectorAll('a');
 
-function toggleNav(){
-    if (nav.className == 'closed' && /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)){
+function toggleNav() {
+    if (nav.className == 'closed' && /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         nav.className = 'open';
     } else {
         nav.className = 'closed';
@@ -17,7 +17,7 @@ toggle.addEventListener('click', (event) => {
 
 nav.addEventListener('click', (event) => {
     for (let i = 0; i < navItems.length; i++) {
-        if (event.target == navItems[i]){
+        if (event.target == navItems[i]) {
             toggleNav();
         }
     }
