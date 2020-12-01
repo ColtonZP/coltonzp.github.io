c3c3e3481d04072cbe5483e2ae71d8fb8117fef8
Colton <czpemberton@gmail.com>
2020-11-30 18:51:18 -0800

    react init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,40 @@
 module.exports = {
-    root: true,
-    parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module',
-        ecmaFeatures: {
-            jsx: true,
-        },
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    settings: {
-        react: {
-            version: 'detect',
-        },
+  },
+  settings: {
+    react: {
+      version: 'detect',
     },
-    env: {
-        browser: true,
-        amd: true,
-        node: true,
-    },
-    extends: [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:jsx-a11y/recommended',
-        'plugin:prettier/recommended',
+  },
+  env: {
+    browser: true,
+    amd: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+    'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
     ],
-    plugins: ['simple-import-sort'],
-    rules: {
-        'prettier/prettier': ['error', {}, { usePrettierrc: true }],
-        'react/react-in-jsx-scope': 'off',
-        'jsx-a11y/anchor-is-valid': [
-            'error',
-            {
-                components: ['Link'],
-                specialLink: ['hrefLeft', 'hrefRight'],
-                aspects: ['invalidHref', 'preferButton'],
-            },
-        ],
-        'react/prop-types': 0,
-    },
+    'react/prop-types': 0,
+  },
 }
