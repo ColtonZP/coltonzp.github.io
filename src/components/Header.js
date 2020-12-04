13aed44cd51984d2d7fae7ff10266cf878c95229
Colton <czpemberton@gmail.com>
2020-12-03 16:53:17 -0800

    content restructoring

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,12 @@ import spaceNeedle from '../images/space_needle.svg'
 export default function Header() {
   return (
     <header className="appHeader">
-      <div className="headerTitles">
-        <div className="container">
-          <img src={spaceNeedle} alt="Space Needle" />
-          <h1>
-            <b>Colton</b> Pemberton
-          </h1>
-          <h2>Web developer based in Seattle, Wa.</h2>
-        </div>
+      <img src={spaceNeedle} alt="Space Needle" />
+      <div className="container">
+        <h1>
+          <b>Colton</b> Pemberton
+        </h1>
+        <h2>Web developer based in Seattle, Wa.</h2>
       </div>
     </header>
   )
