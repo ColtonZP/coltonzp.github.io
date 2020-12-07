bc207cce8c7e00681119eb3ddc2dcf1d9837f916
Colton <czpemberton@gmail.com>
2020-12-07 14:37:27 -0800

    style, image and content update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,23 +4,27 @@ import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 import Contact from './Contact'
 import bottomWave from '../images/bottom_wave.svg'
+import mail from '../images/mail.svg'
 
 function Footer() {
   return (
     <footer>
       <img className="wave" src={bottomWave} alt="" />
       <div className="content">
-        <div className="email container">
+        <div className="container">
           <h1>Get in touch!</h1>
-          <Contact />
-        </div>
-        <div className="links container">
-          <a href="https://github.com/ColtonZP" className="icon">
-            <FontAwesomeIcon icon={faGithubSquare} />
-          </a>
-          <a href="https://www.linkedin.com/in/colton-pemberton-1a7292163/" className="icon">
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
+          <div className="email">
+            <Contact />
+            <img className="mail" src={mail} alt="mailing" />
+          </div>
+          <div className="links">
+            <a href="https://github.com/ColtonZP" className="icon">
+              <FontAwesomeIcon icon={faGithubSquare} />
+            </a>
+            <a href="https://www.linkedin.com/in/colton-pemberton-1a7292163/" className="icon">
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
