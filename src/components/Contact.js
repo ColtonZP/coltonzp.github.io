e7a7f0cdf8a33b6adad6f301c8e852b2d8805b94
Colton <czpemberton@gmail.com>
2020-12-14 12:51:39 -0800

    more form fixes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ContactUs() {
   return (
-    <form className="contactForm" action="POST" netlify-honeypot="bot-field" data-netlify="true">
+    <form className="contactForm" name="contact" method="post">
+      <input type="hidden" name="form-name" value="contact" />
       <input className="name" type="text" name="name" placeholder="Name" required />
-      <input className="number" type="tel" name="contact_number" placeholder="Phone" />
       <input className="email" type="email" name="email" placeholder="Email" required />
       <textarea className="message" name="message" placeholder="Message" required />
       <div data-netlify-recaptcha="true"></div>
