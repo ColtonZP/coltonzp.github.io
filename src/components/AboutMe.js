1b522a8f7a40d0e88cf7ec4a4af48701dc919913
Colton <czpemberton@gmail.com>
2021-02-26 09:27:21 -0800

    style updates

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,39 +1,14 @@
 import React from 'react'
 import computer_work from '../images/computer_work.svg'
-import computer from '../images/computer.svg'
-import responsive from '../images/responsive_design.svg'
-import qa from '../images/qa.svg'
 
-const empSkills = [
-  {
-    title: 'Web Development',
-    class: 'WebDevelopment',
-    desc:
-      "A strong skillset and endless eagerness to learn something new, it's easy to get any project built from the ground up and deployed to the web.",
-    img: computer,
-  },
-  {
-    title: 'Responsive',
-    class: 'Responsive',
-    desc:
-      'The rapid increase in mobile traffic, it’s no question everything should be built from the ground up with mobile in mind.',
-    img: responsive,
-  },
-  {
-    title: 'Quality Assurance',
-    class: 'QualityAssurance',
-    desc:
-      'Striving for a pixel perfect site is only on the surface, nothing should be left out; I create the cleanest code that is bug-free and great performance.',
-    img: qa,
-  },
-]
+import { empSkills } from '../content/skills'
 
 const AboutMe = () => (
   <div className="aboutMe container">
     <div className="blurb">
       <div className="text">
         <h3>
-          Hello, there!{' '}
+          Hello, there!
           <span role="img" aria-label="wave">
             👋
           </span>
--- src/content/skills.js
+++ src/content/skills.js
@@ -1,3 +1,7 @@
+import computer from '../images/computer.svg'
+import responsive from '../images/responsive_design.svg'
+import qa from '../images/qa.svg'
+
 export const skills = [
   'HTML',
   'CSS',
@@ -16,4 +20,28 @@ export const skills = [
   'Firebase',
   'Strapi',
   'Prismic',
+]
+
+export const empSkills = [
+  {
+    title: 'Web Development',
+    class: 'WebDevelopment',
+    desc:
+      "A strong skillset and endless eagerness to learn something new, it's easy to get any project built from the ground up and deployed to the web.",
+    img: computer,
+  },
+  {
+    title: 'Responsive',
+    class: 'Responsive',
+    desc:
+      'The rapid increase in mobile traffic, it’s no question everything should be built from the ground up with mobile in mind.',
+    img: responsive,
+  },
+  {
+    title: 'Quality Assurance',
+    class: 'QualityAssurance',
+    desc:
+      'Striving for a pixel perfect site is only on the surface, nothing should be left out; I create the cleanest code that is bug-free and great performance.',
+    img: qa,
+  },
 ]
