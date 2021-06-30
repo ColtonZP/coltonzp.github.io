9a07f6690aafde8571dfbcc61cd5fe25560205b6
Colton <czpemberton@gmail.com>
2021-06-29 22:48:05 -0700

    new header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,52 +1,19 @@
-import { useState, useEffect } from 'react'
-
-import Prismic from 'prismic-javascript'
-
-import { Client } from '../prismic-configuration'
-
 export function Header() {
-    const [resume, updateResume] = useState(null)
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await Client.query(Prismic.Predicates.at('document.type', 'file'))
-            if (response) {
-                updateResume(response.results[0].data.resume.url)
-                console.log(resume)
-            }
-        }
-        fetchData()
-    }, [])
-
     return (
-        <header className="appHeader">
+        <header className="app-header">
             <div className="container">
-                <div className="content">
-                    <div className="headline">
-                        <h1>
-                            Colton <b>Pemberton</b>
-                        </h1>
-                        <p>Full stack Java/React.js Developer</p>
-                        <span className="hashtag">#openforwork</span>
-                    </div>
+                <h1>
+                    Colton <b>Pemberton</b>
+                </h1>
+                <h2 className="tag-line">Full Stack Developer</h2>
 
-                    <div className="about">
-                        <h3>
-                            Hello, there!{' '}
-                            <span role="img" aria-label="wave">
-                                👋
-                            </span>
-                        </h3>
-                        <p>
-                            I&#39;m Colton, I spend a lot of time writing code. I work hard to make the internet a
-                            better place than when I found it. I&#39;m seeking a full time position in web development.
-                        </p>
-                        {resume && (
-                            <a className="Download" href={String(resume)} download>
-                                Download Resume
-                            </a>
-                        )}
-                    </div>
-                </div>
+                <p className="about">
+                    I like to build projects using <span>Java</span> and <span>React.js</span>. I work hard to make the
+                    internet a better place than when I found it. I&#39;m currently seeking a full time position in web
+                    development.
+                </p>
+
+                <a>Get in touch</a>
             </div>
         </header>
     )
